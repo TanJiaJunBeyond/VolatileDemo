c196a6d4cd0735a1837fb7d733665cfe3b3f3cf7
谭嘉俊 <1120571286@qq.com>
2020-08-23 20:52:44 +0800

    添加内存屏障测试代码。

---
+++ src/MemoryBarrierTest.java
@@ -0,0 +1,31 @@
+/**
+ * Created by TanJiaJun on 2020/8/23.
+ */
+class MemoryBarrierTest {
+
+    private int a, b;
+    private volatile int c, d;
+
+    private void test() {
+        int i, j;
+        i = a; // load a
+        j = b; // load b
+        i = c; // load c
+        // LoadLoad
+        j = d; // load d
+        // LoadStore
+        a = i; // store a
+        b = j; // store b
+        // StoreStore
+        c = i; // store c
+        // StoreStore
+        d = j; // store d
+        // StoreLoad
+        i = d; // load d
+        // LoadLoad
+        // LoadStore
+        j = b; // load b
+        a = i; // store a
+    }
+
+}
